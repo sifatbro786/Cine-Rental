a54daf41024ce51a70e117bb49463b5b0cb98e34
sifatbro786 <solaimanislamsifat@gmail.com>
2024-10-23 00:11:46 +0600

    Footer section complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import Footer from "./HomePage/Footer";
 import Header from "./HomePage/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          
+        </div>
+      </main>
+      <Footer />
     </>
   );
 }
