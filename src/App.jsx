e778ceca2ec68dd61d82547c35fc1213dc7235fb
sifatbro786 <solaimanislamsifat@gmail.com>
2024-10-23 00:15:38 +0600

    Sidebar Component complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Footer from "./HomePage/Footer";
 import Header from "./HomePage/Header";
+import Sidebar from "./HomePage/Sidebar";
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <Header />
       <main>
         <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
-          
+          <Sidebar />
         </div>
       </main>
       <Footer />
